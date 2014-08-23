4e3a3a042b7309ee70b3ac87ee58d71307ba507e
Jayin Ton <tonjayin@gmail.com>
2014-08-23 21:58:05 +0800

    ~

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ define('APP_DEBUG',True);
 // 定义应用目录
 define('APP_PATH','./App/');
 
+//create and set the fault
+// define('BIND_MODULE','Admin');
+
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
